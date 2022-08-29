c8e58cc6f28f0c7c86f59c4661de6e1316a49fa2
Victor Ribeiro <devjvribeiro@gmail.com>
2022-08-29 08:46:06 -0300

    fix: removed non existent route

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,14 +1,8 @@
 import HomePage from '@pages/index'
-import SobrePage from '@pages/sobre'
 
 export default [
   {
     path: '/',
     component: HomePage,
-  },
-
-  {
-    path: '/sobre',
-    component: SobrePage,
   },
 ]
