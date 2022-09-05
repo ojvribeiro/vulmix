27ca1352910b6c1ed3704824558a9607a072cbdb
Victor Ribeiro <devjvribeiro@gmail.com>
2022-09-05 02:32:15 -0300

    Fixed Tailwind scanning paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    './index.html',
+    './src/**/pages/*.{vue,html,js}',
+    './src/**/components/*.{vue,html,js}',
+  ],
   theme: {
     extend: {},
   },
