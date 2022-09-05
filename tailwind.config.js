c59ac6898a9002fd230caf8efc3f01f289545244
Victor Ribeiro <devjvribeiro@gmail.com>
2022-09-05 03:29:37 -0300

    Added `.vue-mix` folder to Tailwind's folder scan
    According to #18.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   content: [
     './index.html',
+    './.vue-mix/**/pages/*.{vue,html,js}',
+    './.vue-mix/**/components/*.{vue,html,js}',
     './src/**/pages/*.{vue,html,js}',
     './src/**/components/*.{vue,html,js}',
   ],
