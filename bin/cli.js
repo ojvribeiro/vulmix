d5d1d613bf196cb7851ba54ed185f40bb0724fa2
Victor Ribeiro <devjvribeiro@gmail.com>
2022-09-06 04:50:49 -0300

    Removes the `bin` folder on install

--- bin/cli.js
+++ bin/cli.js
@@ -59,6 +59,7 @@ async function main() {
 
     console.log('\n\n🗑️ Removing useless files...\n\n')
     execSync('npx rimraf ./.git')
+    fs.rmdirSync(path.join(projectPath, 'bin'), { recursive: true })
 
     console.log(
       // Green
