7dc948dfee4f4d6706b0f88a59dcb6f015fee9ab
Victor Ribeiro <devjvribeiro@gmail.com>
2022-09-28 04:43:04 -0300

    Moved basic head tags to `app.vue`

--- app.vue
+++ app.vue
@@ -3,6 +3,9 @@
     <title>
       Vue + Laravel Mix = Vulmix
     </title>
+
+    <meta name="title" content="Your application title">
+    <meta name="description" content="Your application description">
   </Head>
 
   <App />
