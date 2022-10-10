bf6c776a61c200e59e2328e57ac1fb27fe98cc6e
Victor Ribeiro <devjvribeiro@gmail.com>
2022-10-10 20:08:21 -0300

    fix(starter-template): added head

--- src/components/VulmixWelcome.vue
+++ src/components/VulmixWelcome.vue
@@ -1,4 +1,15 @@
 <template>
+  <Head>
+    <title>
+      Vue + Laravel Mix = Vulmix
+    </title>
+
+    <meta name="title" content="Your application title">
+    <meta name="description" content="Your application description">
+
+    <link rel="icon" href="/assets/icons/favicon.png">
+  </Head>
+
   <section class="vulmix-welcome">
     <div>
       <div class="vulmix-logo">
