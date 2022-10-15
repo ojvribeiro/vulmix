4cfde30fe2b99ae7de995d3fbb4588231d845dcb
Victor Ribeiro <devjvribeiro@gmail.com>
2022-10-15 18:18:03 -0300

    refactor(core): pure CSS instead of Tailwind

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,7 +1,30 @@
 <template>
-  <div className="grid grid-cols-12 px-6 md:px-12 2xl:px-16 3xl:px-0">
-    <div className="col-span-12 3xl:col-span-10 3xl:col-start-2">
-      <slot />
-    </div>
+  <div class="vx-container">
+    <slot />
   </div>
 </template>
+
+<style>
+  .vx-container {
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+    max-width: 80rem/* 1280px */;
+    padding-left: 1rem/* 16px */;
+    padding-right: 1rem/* 16px */;
+  }
+
+  @media (min-width: 640px) {
+    .vx-container {
+      padding-left: 1.5rem/* 24px */;
+      padding-right: 1.5rem/* 24px */;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .vx-container {
+      padding-left: 2rem/* 32px */;
+      padding-right: 2rem/* 32px */;
+    }
+  }
+</style>
--- src/pages/404.vue
+++ src/pages/404.vue
@@ -5,13 +5,39 @@
     </title>
   </Head>
 
-  <div class="grid place-items-center h-full text-center bg-zinc-800">
-    <div class="space-y-4">
-      <h1 class="text-8xl text-blue-200 font-black">404</h1>
+  <div class="vx-page-outter">
+    <div class="vx-page-inner">
+      <h1>404</h1>
 
-      <p class="text-blue-400">
+      <p>
         <Link to="/">Return to Home</Link>
       </p>
     </div>
   </div>
 </template>
+
+<style scoped>
+  .vx-page-outter {
+    display: grid;
+    place-items: center;
+    height: 100%;
+    text-align: center;
+    background-color: #27272a;
+  }
+
+  .vx-page-outter .vx-page-inner {
+    margin-top: calc(1rem * calc(1 - 0));
+    margin-bottom: calc(1rem * 0);
+  }
+
+  .vx-page-outter .vx-page-inner h1 {
+    font-size: 6rem/* 96px */;
+    line-height: 1;
+    color: #bfdbfe;
+    font-weight: 900;
+  }
+
+  .vx-page-outter .vx-page-inner p {
+    color: #60a5fa;
+  }
+</style>
