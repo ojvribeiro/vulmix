17edf65dfb1f5fa2b23f896e17518c4f6ffcd085
Victor Ribeiro <devjvribeiro@gmail.com>
2022-11-21 05:05:28 -0300

    feat(core/demo): added demo placeholder images

---
+++ demo/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <Image src="/assets/img/140x100.png" width="140" height="100" />
+  <Image src="/assets/img/200x100.png" width="200" height="100" />
+  <Image src="/assets/img/350x150.png" width="350" height="150" />
+  <Image src="/assets/img/sample.jpg" width="1080" height="1920" />
+</template>
