aa55a2e0936a656c1b1b1baba988226845fd832c
Victor Ribeiro <devjvribeiro@gmail.com>
2023-01-27 16:53:55 -0300

    progress: Typescript support

---
+++ src/main.ts
@@ -0,0 +1,150 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createHead } from '@vueuse/head';
+
+import App from '~/app.vue';
+
+const app = createApp(App);
+const head = createHead();
+
+app.component('App', require('@/App.vue').default);
+
+/**
+ * Built-in components
+ */
+const nativeComponents = require.context(
+  '@/components/',
+  true,
+  /\.(vue|js|ts)$/i
+);
+nativeComponents.keys().map((key: string) => {
+  let nativeComponentName = key.split('.')[1].replace(/\//g, '');
+
+  if (nativeComponentName.match(/index$/)) {
+    nativeComponentName = nativeComponentName.replace('index', '');
+  }
+
+  app.component(nativeComponentName, nativeComponents(key).default);
+});
+
+/**
+ * Components
+ */
+const componentFiles = require.context('@components/', true, /\.(vue|js|ts)$/i);
+componentFiles.keys().map((key: string) => {
+  let componentName = key.split('.')[1].replace(/\//g, '');
+
+  if (componentName.match(/index$/)) {
+    componentName = componentName.replace('index', '');
+  }
+
+  app.component(componentName, componentFiles(key).default);
+});
+
+let routes: Array<RouteRecordRaw> = [];
+
+/**
+ * Built-in pages
+ */
+const nativePageComponents = require.context(
+  '@/pages/',
+  true,
+  /\.(vue|js|ts)$/i
+);
+nativePageComponents.keys().map((key: string) => {
+  let slugName = key
+    .split('.')[1]
+    .replace(/([A-Z])/g, '-$1')
+    .replace(/(^-)/g, '')
+    .toLowerCase();
+
+  if (slugName.match(/\/index$/)) {
+    slugName = slugName.replace('/index', '/');
+  }
+
+  routes.push({
+    path: slugName === '/index' ? '/' : `/${slugName}`,
+    component: nativePageComponents(key).default,
+  });
+});
+
+/**
+ * Pages
+ */
+const pageComponents = require.context('@pages/', true, /\.(vue|js|ts)$/i);
+pageComponents.keys().map((key: string) => {
+  let slugName = key
+    .split('.')[1]
+    .replace(/([A-Z])/g, '-$1')
+    .replace(/(^-)/g, '')
+    .toLowerCase();
+
+  if (slugName.match(/\/index$/)) {
+    slugName = slugName.replace('/index', '/');
+  }
+
+  routes.push({
+    path: slugName === '/index' ? '/' : `/${slugName}`,
+    component: pageComponents(key).default,
+  });
+});
+
+/**
+ * Dynamic Pages
+ */
+const dynamicPageComponents = require.context(
+  '@pages/',
+  true,
+  /\[(.*)\]\.(vue|js|ts)$/i
+);
+dynamicPageComponents.keys().map((key: string) => {
+  let slugName = key
+    .split('.')[1]
+    .replace(/([A-Z])/g, '-$1')
+    .replace(/(^-)/g, '')
+    .toLowerCase();
+
+  if (slugName.match(/\/index$/)) {
+    slugName = slugName.replace('/index', '/');
+  } else {
+    slugName = slugName.replace(/\/\[(.*)\]/, '/:$1');
+  }
+
+  routes.push({
+    path: slugName === '/index' ? '/' : `/${slugName}`,
+    component: dynamicPageComponents(key).default,
+  });
+});
+
+routes.push({
+  path: '/:pathMatch(.*)*',
+  component: require('@/pages/404.vue').default,
+});
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+});
+
+/**
+ * Layouts
+ */
+const layoutFiles = require.context('@layouts/', true, /\.(vue|js|ts)$/i);
+
+layoutFiles.keys().map((key: string) => {
+  const layoutName =
+    'layout-' +
+    key
+      .split('.')[1]
+      .replace(/\//g, '')
+      .replace(/([A-Z])/g, '-$1')
+      .replace(/(^-)/g, '')
+      .toLowerCase();
+
+  app.component(layoutName, layoutFiles(key).default);
+});
+
+app.use(router);
+app.use(head);
+
+app.mount('[data-vulmix-app]');
