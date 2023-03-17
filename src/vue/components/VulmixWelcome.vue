2af359f8ac008eedcd9dd3a1365354204b6dfb81
Victor Ribeiro <devjvribeiro@gmail.com>
2023-03-17 05:54:10 -0300

    fix(components/VulmixWelcome): adjustment in default title

--- src/vue/components/VulmixWelcome.vue
+++ src/vue/components/VulmixWelcome.vue
@@ -1,11 +1,6 @@
 <template>
   <Head>
-    <title>Vue + Laravel Mix = Vulmix</title>
-
-    <meta name="title" content="Your application title" />
-    <meta name="description" content="Your application description" />
-
-    <link rel="icon" href="/assets/icons/favicon.png" />
+    <title>New Vulmix app</title>
   </Head>
 
   <section class="vulmix-welcome">
