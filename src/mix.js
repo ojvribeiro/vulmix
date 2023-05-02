bf262ee4c43acfc9b7bd7a9c034fbf29a871d890
Victor Ribeiro <devjvribeiro@gmail.com>
2023-05-01 21:56:33 -0300

    chore(core): modularized Unplugin config

--- src/mix.js
+++ src/mix.js
@@ -6,6 +6,7 @@ const { argv } = require('yargs')
 const pkg = require('../package.json')
 const { useConsole } = require('./utils/console.js')
 const getRelativePath = require('./utils/getRelativePath.js')
+const { UnpluginAutoImports } = require('./config/unplugin.js')
 const {
   absoluteVulmixPaths,
   relativeVulmixPaths,
@@ -86,93 +87,7 @@ class VulmixInit {
       })
 
       .webpackConfig({
-        plugins: [
-          require('unplugin-vue-components/webpack')({
-            /* options */
-
-            // Vue version of project. It will detect automatically if not specified.
-            // Acceptable value: 2 | 2.7 | 3
-            version: 3,
-
-            // relative paths to the directory to search for components.
-            dirs: [
-              `${absoluteRootPath}/components`,
-              `${absolutePackagePath}/src/vue/components/**`,
-            ],
-
-            // valid file extensions for components.
-            extensions: ['vue', 'ts', 'js'],
-
-            // Allow subdirectories as namespace prefix for components.
-            directoryAsNamespace: true,
-
-            // Collapse same prefixes (camel-sensitive) of folders and components
-            // to prevent duplication inside namespaced component name.
-            // works when `directoryAsNamespace: true`
-            collapseSamePrefixes: true,
-
-            // generate `components.d.ts` global declarations,
-            // also accepts a path for custom filename
-            // default: `true` if package typescript is installed
-            dts:
-              options.dev === true
-                ? `${absolutePackagePath}/types/components.d.ts`
-                : `${absoluteRootPath}/.vulmix/types/components.d.ts`,
-          }),
-
-          require('unplugin-auto-import/webpack')({
-            // targets to transform
-            include: [
-              /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-              /\.vue$/,
-              /\.vue\?vue/, // .vue
-              /\.md$/, // .md
-            ],
-
-            // global imports to register
-            imports: [
-              // presets
-              'vue',
-              'vue-router',
-              // custom
-              {
-                '@vueuse/core': [
-                  // named imports
-                  'useFetch', // import { useMouse } from '@vueuse/core',
-                ],
-              },
-            ],
-            // Enable auto import by filename for default module exports under directories
-            defaultExportByFilename: false,
-
-            // Auto import for module exports under directories
-            // by default it only scan one level of modules under the directory
-            dirs: [
-              // './hooks',
-              // './composables' // only root modules
-              // './composables/**', // all nested modules
-              `${absoluteRootPath}/composables/**`, // all nested modules
-            ],
-
-            // Filepath to generate corresponding .d.ts file.
-            // Defaults to './auto-imports.d.ts' when `typescript` is installed locally.
-            // Set `false` to disable.
-            dts:
-              options.dev === true
-                ? `${absolutePackagePath}/types/auto-imports.d.ts`
-                : `${absoluteRootPath}/.vulmix/types/auto-imports.d.ts`,
-
-            // Auto import inside Vue template
-            // see https://github.com/unjs/unimport/pull/15 and https://github.com/unjs/unimport/pull/72
-            vueTemplate: true,
-
-            // Custom resolvers, compatible with `unplugin-vue-components`
-            // see https://github.com/antfu/unplugin-auto-import/pull/23/
-            resolvers: [
-              /* ... */
-            ],
-          }),
-        ],
+        plugins: [...UnpluginAutoImports(isDevMode)],
 
         resolve: {
           extensions: ['.js', '.vue', '.ts'],
