380cab0ea6a186660aa6db4cb20ddc4a28a3a55d
Victor Ribeiro <devjvribeiro@gmail.com>
2023-05-05 13:35:53 -0300

    feat(types): added autocomplete for head tags

--- types/vulmix-config.d.ts
+++ types/vulmix-config.d.ts
@@ -1,21 +1,128 @@
 export interface VulmixConfig {
-  lang?: string
+  lang?:
+    | 'en'
+    | 'pt-BR'
+    | 'es'
+    | 'fr'
+    | 'de'
+    | 'it'
+    | 'pt'
+    | 'ru'
+    | 'ja'
+    | 'ko'
+    | 'zh'
 
   head?: {
     title?: string
 
     meta?: {
-      charset?: string
-      name?: string
+      charset?: 'utf-8' | 'iso-8859-1' | 'iso-8859-2' | 'utf-16'
+      name?:
+        | 'viewport'
+        | 'description'
+        | 'keywords'
+        | 'author'
+        | 'generator'
+        | 'robots'
+        | 'googlebot'
+        | 'google'
+        | 'bingbot'
+        | 'bing'
+        | 'yandexbot'
+        | 'yandex'
+        | 'yandex-verification'
+        | 'apple-mobile-web-app-title'
+        | 'application-name'
+        | 'msapplication-TileColor'
+        | 'theme-color'
+        | 'msapplication-TileImage'
+        | 'msapplication-config'
+        | 'mobile-web-app-capable'
+        | 'apple-mobile-web-app-capable'
+        | 'apple-mobile-web-app-status-bar-style'
+        | 'format-detection'
+        | 'apple-touch-fullscreen'
+        | 'og:title'
+        | 'og:description'
+        | 'og:image'
+        | 'og:url'
+        | 'og:site_name'
+        | 'og:locale'
+        | 'og:type'
+        | 'og:audio'
+        | 'og:video'
+        | 'og:determiner'
+        | 'og:updated_time'
+        | 'og:locale:alternate'
+        | 'twitter:card'
+        | 'twitter:site'
+        | 'twitter:site:id'
+        | 'twitter:creator'
+        | 'twitter:creator:id'
+        | 'twitter:title'
+        | 'twitter:description'
+        | 'twitter:image'
+        | 'twitter:image:alt'
+        | 'twitter:player'
+        | 'twitter:player:width'
+        | 'twitter:player:height'
+        | 'twitter:player:stream'
+        | 'twitter:player:stream:content_type'
+        | 'twitter:app:name:iphone'
+        | 'twitter:app:id:iphone'
+        | 'twitter:app:url:iphone'
+        | 'twitter:app:name:ipad'
+        | 'twitter:app:id:ipad'
+        | 'twitter:app:url:ipad'
+        | 'twitter:app:name:googleplay'
+        | 'twitter:app:id:googleplay'
+        | 'twitter:app:url:googleplay'
+        | 'twitter:label1'
+        | 'twitter:data1'
+        | 'twitter:label2'
+        | 'twitter:data2'
+        | 'fb:app_id'
+        | 'fb:pages'
+        | 'fb:admins'
+        | 'fb:profile_id'
+        | 'fb:article_style'
+        | 'fb:use_automatic_ad_placement'
+        | 'fb:enable_native_login'
+
       property?: string
-      'http-equiv'?: string
+      'http-equiv'?: 'X-UA-Compatible' | 'Content-Type' | 'Content-Language'
       content?: string
     }[]
 
     link?: {
-      rel?: string
-      sizes?: string
+      rel?:
+        | 'stylesheet'
+        | 'icon'
+        | 'apple-touch-icon'
+        | 'mask-icon'
+        | 'canonical'
+        | 'manifest'
+        | 'author'
+        | 'licence'
+        | 'alternate'
+        | 'me'
+        | 'dns-prefetch'
+        | 'preconnect'
+        | 'prefetch'
+        | 'prerender'
+        | 'preload'
+        | 'modulepreload'
+
+      sizes?: '16x16' | '32x32' | '96x96' | '192x192'
       href?: string
+      hreflang?: 'en' | 'es'
+    }[]
+
+    script?: {
+      type?: 'text/javascript' | 'application/javascript' | 'module'
+      src?: string
+      async?: boolean
+      defer?: boolean
     }[]
   }
 }
