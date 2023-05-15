cad351cf9fd9f77827c0831b17227d8156b898a7
Victor Ribeiro <devjvribeiro@gmail.com>
2023-05-15 15:08:34 -0300

    feat(components/Link): created `href` alias for `to` prop

--- src/vue/components/Link.vue
+++ src/vue/components/Link.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
-  defineProps<{
-    to: string
+  import { RouterLink } from 'vue-router'
+
+  const props = defineProps<{
+    to?: string
+    href?: string
   }>()
 </script>
 
 <template>
-  <RouterLink :to="to">
+  <RouterLink v-if="props.to && !props.href" :to="props.to">
     <slot />
   </RouterLink>
+
+  <RouterLink v-else-if="props.href && !props.to" :to="props.href">
+    <slot />
+  </RouterLink>
+
+  <RouterLink v-else-if="props.href && props.to" :to="props.to">
+    <slot />
+  </RouterLink>
+
+  <a v-else>
+    <slot />
+  </a>
 </template>
