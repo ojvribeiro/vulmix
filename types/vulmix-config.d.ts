2794c491680ad87478a8d9c4f816b9ca083132a8
Victor Ribeiro <devjvribeiro@gmail.com>
2023-06-04 15:45:11 -0300

    feat: added `type` in the `link` option

--- types/vulmix-config.d.ts
+++ types/vulmix-config.d.ts
@@ -110,6 +110,34 @@ export interface VulmixConfig {
       sizes?: string
       href?: string
       hreflang?: string
+      type?:
+        | 'text/css'
+        | 'text/plain'
+        | 'text/html'
+        | 'text/javascript'
+        | 'application/javascript'
+        | 'application/json'
+        | 'application/manifest+json'
+        | 'application/rss+xml'
+        | 'application/atom+xml'
+        | 'application/pdf'
+        | 'application/zip'
+        | 'application/x-font-ttf'
+        | 'application/x-font-opentype'
+        | 'application/x-font-woff'
+        | 'application/x-font-woff2'
+        | 'application/x-font-eot'
+        | 'application/x-font-truetype'
+        | 'image/svg+xml'
+        | 'image/x-icon'
+        | 'image/png'
+        | 'image/jpeg'
+        | 'image/gif'
+        | 'image/webp'
+        | 'image/avif'
+        | 'image/apng'
+        | 'image/bmp'
+        | 'image/vnd.microsoft.icon'
     }[]
 
     script?: {
