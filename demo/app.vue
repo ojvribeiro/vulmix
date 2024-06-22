4dbee1880fa94a2b6144c4013154e376345e72b9
Victor Ribeiro <devjvribeiro@gmail.com>
2024-06-22 19:28:21 -0300

    chore: revert default playground to VulmixWelcome component

--- demo/app.vue
+++ demo/app.vue
@@ -1,17 +1,3 @@
 <template>
-  <div>
-    <h1>Hello</h1>
-
-    <button @click="show = !show">Toggle</button>
-
-    <div v-if="show">
-      <h2>World</h2>
-    </div>
-  </div>
+  <VulmixWelcome />
 </template>
-
-<script setup lang="ts">
-  import { ref } from 'vue'
-
-  const show = ref(true)
-</script>
