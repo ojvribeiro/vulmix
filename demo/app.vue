434607ecc66c9f023f8c1018a7f2d926004e393a
Victor Ribeiro <devjvribeiro@gmail.com>
2024-06-22 19:09:54 -0300

    chore(deps): upgrade Vue to 3.4 and update its dependencies

--- demo/app.vue
+++ demo/app.vue
@@ -1,3 +1,17 @@
 <template>
-  <VulmixWelcome />
+  <div>
+    <h1>Hello</h1>
+
+    <button @click="show = !show">Toggle</button>
+
+    <div v-if="show">
+      <h2>World</h2>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const show = ref(true)
+</script>
