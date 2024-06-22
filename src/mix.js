311289543544e976a6cbc63c5358d338098335c1
Victor Ribeiro <devjvribeiro@gmail.com>
2024-06-22 19:27:07 -0300

    fix: use Vue's Compile-Time Flags

--- src/mix.js
+++ src/mix.js
@@ -83,6 +83,15 @@ class VulmixInit {
       .webpackConfig(webpack => {
         return {
           plugins: [
+            new webpack.DefinePlugin({
+              __VUE_OPTIONS_API__: 'true',
+              __VUE_PROD_DEVTOOLS__: 'false',
+              __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+            }),
+            ...UnpluginAutoImports(),
+            ...(VulmixConfig.webpackConfig?.plugins || []),
+            new ForkTsCheckerWebpackPlugin(),
+          ],
 
           output: {
             assetModuleFilename: pathData => {
